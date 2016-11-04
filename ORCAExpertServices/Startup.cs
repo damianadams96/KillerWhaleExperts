91d569b799c3aba98d42e73cb3236016c986664d
Damian Adams <DamianAdams96@hotmail.com>
2016-11-04 15:28:12 -0400

    Add project files.

---
+++ ORCAExpertServices/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ORCAExpertServices.Startup))]
+namespace ORCAExpertServices
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
