a346c20c10ca965ecb7a85e32a1a4a505c71d8db
Damian Adams <DamianAdams96@hotmail.com>
2016-12-02 18:20:23 -0500

    Expert Controllers and Views Created.

---
+++ ORCAExpertServices/Controllers/ExpertsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ORCAExpertServices.Models;
+
+namespace ORCAExpertServices.Controllers
+{
+    public class ExpertsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Experts
+        public ActionResult Index()
+        {
+            var applicationUsers = db.ApplicationUsers.Include(e => e.ListExpertise);
+            return View(applicationUsers.ToList());
+        }
+
+        // GET: Experts/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expert expert = db.ApplicationUsers.Find(id);
+            if (expert == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expert);
+        }
+
+        // GET: Experts/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id = new SelectList(db.ListExpertises, "ExpertID", "ExpertID");
+            return View();
+        }
+
+        // POST: Experts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,IsAnExpert,Validated")] Expert expert)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ApplicationUsers.Add(expert);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id = new SelectList(db.ListExpertises, "ExpertID", "ExpertID", expert.Id);
+            return View(expert);
+        }
+
+        // GET: Experts/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expert expert = db.ApplicationUsers.Find(id);
+            if (expert == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id = new SelectList(db.ListExpertises, "ExpertID", "ExpertID", expert.Id);
+            return View(expert);
+        }
+
+        // POST: Experts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,IsAnExpert,Validated")] Expert expert)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(expert).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id = new SelectList(db.ListExpertises, "ExpertID", "ExpertID", expert.Id);
+            return View(expert);
+        }
+
+        // GET: Experts/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expert expert = db.ApplicationUsers.Find(id);
+            if (expert == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expert);
+        }
+
+        // POST: Experts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Expert expert = db.ApplicationUsers.Find(id);
+            db.ApplicationUsers.Remove(expert);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ORCAExpertServices/Models/IdentityModels.cs
+++ ORCAExpertServices/Models/IdentityModels.cs
@@ -29,5 +29,9 @@ namespace ORCAExpertServices.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<ORCAExpertServices.Models.Expert> ApplicationUsers { get; set; }
+
+        public System.Data.Entity.DbSet<ORCAExpertServices.Models.ListExpertise> ListExpertises { get; set; }
     }
 }
