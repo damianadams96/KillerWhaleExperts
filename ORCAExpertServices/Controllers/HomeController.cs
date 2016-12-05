a93c5d6ec4bcc6b881f8c8f032aceea5d40d6ade
zachary skorupka <zskorupka@hotmail.com>
2016-12-04 21:47:43 -0500

    marshall orca

--- ORCAExpertServices/Controllers/HomeController.cs
+++ ORCAExpertServices/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace ORCAExpertServices.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About:";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Email:";
 
             return View();
         }
